b3638612dfbafa29b7d2f4706517e12d41e999a4
SamanSurafel17 <samanmobile168@gmail.com>
2022-04-19 11:32:29 -0700

    first printf team work

--- _printf.c
+++ _printf.c
@@ -1,46 +1,111 @@
 #include "main.h"
-
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
- */
-#include "main.h"
-
-/**
- * _printf - Function that prints formatted output.
- *
- * @format: a string composed of zero or more characters to print or use as
- * directives that handle subsequent arguments and special characters.
- *
- * Description: This function can take a variable number and type of arguments
- * that should be printed to standard output.
- *
- * Return: int
+ *_printf - printf
+ *@format: const char pointer
+ *Description: this functions implement some functions of printf
+ *Return: num of characteres printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, chars_printed = 0;
+	const char *string;
+	int cont = 0;
+	va_list arg;
 
-    va_start(args, format);
-    while (format && format[i])
-    {
-        if (format[i] != '%')
-        {
-            chars_printed += _putchar(format[i]);
-        }
-        else if (format[i + 1])
-        {
-            i++;
-            if (format[i] == 'c' || format[i] == 's')
-                chars_printed += format[i] == 'c' ? _putchar(va_arg(args, int)) : print_string(va_arg(args, char *));
+	if (!format)
+		return (-1);
 
-            chars_printed += print_unknown_spec(format[i]);
-        }
-        i++;
-    }
-    va_end(args);
-    return (chars_printed);
+	va_start(arg, format);
+	string = format;
+
+	cont = loop_format(arg, string);
+
+	va_end(arg);
+	return (cont);
+}
+/**
+ *loop_format - loop format
+ *@arg: va_list arg
+ *@string: pointer from format
+ *Description: This function make loop tp string pointer
+ *Return: num of characteres printed
+ */
+int loop_format(va_list arg, const char *string)
+{
+	int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
+
+	while (i < _strlen((char *)string) && *string != '\0')
+	{
+		char aux = string[i];
+
+		if (aux == '%')
+		{
+			i++, flag++;
+			aux = string[i];
+			if (aux == '\0' && _strlen((char *)string) == 1)
+				return (-1);
+			if (aux == '\0')
+				return (cont);
+			if (aux == '%')
+			{
+				flag++;
+			} else
+			{
+				cont_fm = function_manager(aux, arg);
+				if (cont_fm >= 0 && cont_fm != -1)
+				{
+					i++;
+					aux = string[i];
+					if (aux == '%')
+						flag--;
+					cont = cont + cont_fm;
+				} else if (cont_fm == -1 && aux != '\n')
+				{
+					cont += _putchar('%');
+				}
+			}
+		}
+		check_per = check_percent(&flag, aux);
+		cont += check_per;
+		if (check_per == 0 && aux != '\0' && aux != '%')
+			cont += _putchar(aux), i++;
+		check_per = 0;
+	}
+	return (cont);
+}
+/**
+ * check_percent - call function manager
+ *@flag: value by reference
+ *@aux: character
+ *Description: This function print % pear
+ *Return: 1 if % is printed
+ */
+int check_percent(int *flag, char aux)
+{
+	int tmp_flag;
+	int cont = 0;
+
+	tmp_flag = *flag;
+	if (tmp_flag == 2 && aux == '%')
+	{
+		_putchar('%');
+		tmp_flag = 0;
+		cont = 1;
+	}
+	return (cont);
+}
+
+/**
+ * call_function_manager - call function manager
+ *@aux: character parameter
+ *@arg: va_list arg
+ *Description: This function call function manager
+ *Return: num of characteres printed
+ */
+
+int call_function_manager(char aux, va_list arg)
+{
+	int cont = 0;
+
+	cont = function_manager(aux, arg);
+	return (cont);
 }
